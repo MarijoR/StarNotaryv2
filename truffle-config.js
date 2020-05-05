6b9bc054560c3afb3e3237b01bfb9f1f6f1c3eaf
MarijoR <marijo.radman@gmail.com>
2020-05-06 00:20:31 +0200

    Add all my files

---
+++ truffle-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 9545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.6.7",
+      settings: {
+        optimizer: {
+          enabled: true, // Default: false
+          runs: 200      // Default: 200
+        },
+      }
+    }
+  }
+};
